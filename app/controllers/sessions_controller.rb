6fd35a01f5b998441c6c6b6f10c59fd0eb2073d5
tatdat97dhbkhn <tatdat97dhbkhn@gmail.com>
2018-07-12 17:09:40 +0700

    sign-in

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+  def new; end
+
+  def create
+    user = User.find_by email: params[:session][:email].downcase
+    if user&.authenticate params[:session][:password]
+      log_in user
+      remember_forget user
+      redirect_to root_url
+    else
+      flash.now[:danger] = t "errors1"
+      render :new
+    end
+  end
+
+  def destroy
+    log_out if logged_in?
+    redirect_to root_url
+  end
+
+  private
+  def remember_forget user
+    params[:session][:remember_me] == "1" ? remember(user) : forget(user)
+  end
+end
