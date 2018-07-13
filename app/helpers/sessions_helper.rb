86fdf1eb14bdd476364de3ff89f16e25a06adb54
ThuyNguyen <thuynguyennhu97@gmail.com>
2018-07-13 11:29:34 +0700

    sign up

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,23 @@
+module SessionsHelper
+  def log_in user
+    session[:user_id] = user.id
+  end
+
+  def current_user
+    if user_id = session[:user_id]
+      @current_user ||= User.find_by id: user_id
+    elsif user_id = cookies.signed[:user_id]
+      user = User.find_by id: user_id
+      log_in user
+      @current_user = user
+    end
+  end
+
+  def logged_in?
+    current_user.present?
+  end
+
+  def current_user? user
+    user == current_user
+  end
+end
