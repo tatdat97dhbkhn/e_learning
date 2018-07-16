64fc547d639458b9163d182b24a1bd06d7bc87ec
ThuyNguyen <thuynguyennhu97@gmail.com>
2018-07-16 21:41:04 +0700

    update results

--- app/models/lession_log.rb
+++ app/models/lession_log.rb
@@ -3,7 +3,7 @@ class LessionLog < ApplicationRecord
   belongs_to :lession
   has_many :question_logs, dependent: :destroy
 
-  def create_lessionlog
+  def create_lession_log
     category = lession.course.category
     @questions = category.questions.order("RAND()").first Settings.lession.page
 
@@ -13,8 +13,26 @@ class LessionLog < ApplicationRecord
     end
   end
 
+  def update_result question_logs
+    @total = question_logs.keys.count
+    @correct = Settings.number.zero
+
+    question_logs.keys.each do |question_log_id|
+      quession_log = QuestionLog.find_by id: question_log_id
+      quession_log.update_attributes answer_id: question_logs[question_log_id]
+      @correct += Settings.number.one if quession_log.answer.correct
+    end
+
+    if (@correct * Settings.number.one_float / @total) >
+      Settings.number.enought_to_pass
+      update_attributes pass: true
+    else
+      update_attributes pass: false
+    end
+  end
+
   class << self
-    def get_lessionlog question_logs
+    def get_lession_log question_logs
       @questions = []
       @answers = []
 
