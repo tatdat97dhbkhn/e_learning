d0b16620724cfc866a43a19b421a952fa564ac04
tatdat97dhbkhn <tatdat97dhbkhn@gmail.com>
2018-07-17 19:39:41 +0700

    setTime

--- db/migrate/20180712091959_create_lession_logs.rb
+++ db/migrate/20180712091959_create_lession_logs.rb
@@ -2,9 +2,9 @@ class CreateLessionLogs < ActiveRecord::Migration[5.2]
   def change
     create_table :lession_logs do |t|
       t.boolean :pass
+      t.bigint :spend_time, default: 0
       t.references :user, foreign_key: true
       t.references :lession, foreign_key: true
-
       t.timestamps
     end
   end
