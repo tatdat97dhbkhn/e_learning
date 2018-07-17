bca826e34de3139422499a000b1b5d2b6f4b8adf
tatdat97dhbkhn <tatdat97dhbkhn@gmail.com>
2018-07-17 19:39:41 +0700

    progress

--- app/controllers/choose_category_controller.rb
+++ app/controllers/choose_category_controller.rb
@@ -1,7 +1,43 @@
 class ChooseCategoryController < ApplicationController
   skip_before_action :is_admin?
-  
+
   def choose_category
     @categories = Category.all
+    @arr_color = %w(success info warning danger)
+    @number_courses = []
+    @categories.each do |category|
+      @number_courses.push category.courses.count
+    end
+
+    @lession_logs = LessionLog.where user_id: current_user.id, pass: true
+
+    pass_courses = []
+    Category.all.each do |category|
+      category.courses.each do |course|
+        if course.lession_logs.current(current_user).pass_lession.count != 0
+          pass_courses.push Settings.number.one
+        else
+          pass_courses.push Settings.number.zero
+        end
+      end
+    end
+
+    @status_courses = []
+    @number_courses.each do |number_course|
+      @status_courses.push pass_courses[Settings.number.zero...number_course]
+      (Settings.number.zero...number_course).each do |number|
+        pass_courses.delete_at(number)
+      end
+    end
+
+    @result = []
+    @status_courses.each do |status_course|
+      res = status_course.count(Settings.number.one).to_f / status_course.count
+      @result.push res
+    end
+
+    @progress = []
+    @progress.push @categories
+    @progress.push @result
   end
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,5 @@
 class StaticPagesController < ApplicationController
   skip_before_action :is_admin?
-  
+
   def home; end
 end
